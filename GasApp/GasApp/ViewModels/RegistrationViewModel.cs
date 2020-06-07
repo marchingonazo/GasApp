fd120a21da1658f6e77b5e78c3af599719771b16
marchingonazo <41498625+marchingonazo@users.noreply.github.com>
2020-06-06 21:33:08 -0600

    pegpage3

--- GasApp/GasApp/ViewModels/RegistrationViewModel.cs
+++ GasApp/GasApp/ViewModels/RegistrationViewModel.cs
@@ -4,19 +4,32 @@ using System.ComponentModel;
 
 using Xamarin.Forms;
 using System.Windows.Input;
+using System.IO;
 
 namespace GasApp.ViewModels
 {
     public class RegistrationViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
         public ICommand ClicLoginPageCommand { get; set; }
+        public ICommand ClicAddImageCommand { get; set; }
+        public ICommand ClicSafeUserCommand { get; set; }
+
 
         public RegistrationViewModel()
         {
             ClicLoginPageCommand = new Command(async () =>
             {
                 await Application.Current.MainPage.Navigation.PopAsync();
+            });
+
+            ClicAddImageCommand = new Command(() =>
+            {
+                //FileStream pic = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), FileMode.Open);
+                //FileStream piic = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), FileImageSource.);
+               // ImageSource image = ImageSource.FromFile(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
+                //var pic = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "*.jpeg");
             });
         }
         public void SafeUser()
@@ -26,6 +39,16 @@ namespace GasApp.ViewModels
         public void DeleteUser()
         {
 
+        }
+       
+        public async void AddImage()
+        {
+            ClicAddImageCommand = new Command(() =>
+            {
+                //Stream stream = await DependencyService.Get<IphotoPickerService>
+                //FileStream pic = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), FileMode.Open);
+                //var pic = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "*.jpeg");
+            });
         }
 
     }
